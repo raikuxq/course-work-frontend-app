ecd9162872be848ce0ae939d7342c9bd4b799d94
Raiku <raikulmao.dev@gmail.com>
2023-06-19 08:36:48 +0300

    Refactor translations;

--- src/translations/en/channel.en.ts
+++ src/translations/en/channel.en.ts
@@ -1,36 +1,70 @@
 export default {
     list: {
-        title: 'Section List',
-        main: 'Main'
+        title: 'Channel List: ',
+        titleOwn: 'My Channels:',
+        titleMemberOf: 'Member of Channels'
     },
     labels: {
         info: 'Information',
-        name: 'Section Name',
+        name: 'Channel Name',
+        task: 'Task',
         author: 'Author',
         members: 'Members',
-        categories: 'Sections',
+        categories: 'Categories',
+        inviteLink: 'Invitation Code',
         description: 'Description'
     },
     links: {
         task: 'Task',
     },
+    placeholders: {
+        name: '',
+        developers: 'Select developers',
+        locales: 'Select languages',
+        find: 'Search channel by name'
+    },
     form: {
-        create: 'Create',
+        create: 'Create Channel',
+        create_short: 'Create',
+        update: 'Update Channel',
+        update_short: 'Update',
+        join: 'Join Channel',
+        join_short: 'Join',
+        leave: 'Leave Channel',
+        leave_short: 'Leave',
         field: {
             title: {
                 label: 'Title',
                 placeholder: ''
             },
+            desc: {
+                label: 'Description',
+                placeholder: ''
+            },
+            inviteLink: {
+                label: 'Invitation Code',
+                placeholder: ''
+            },
         }
     },
     actions: {
-        create: 'Create Section',
-        delete: 'Delete Section',
-        edit: 'Edit Section'
+        join: 'Join',
+        leave: 'Leave Channel',
+        create: 'Create Channel',
+        update: 'Update Channel',
+        delete: 'Delete Channel',
+        edit: 'Edit Channel'
     },
     notify: {
-        created: 'Section created successfully',
-        updated: 'Section updated successfully',
-        deleted: 'Section deleted successfully'
+        created: 'Channel created successfully',
+        updated: 'Channel updated successfully',
+        deleted: 'Channel deleted successfully',
+        joined: 'You have successfully joined the channel',
+        left: 'You have successfully left the channel',
+        not_created: 'Failed to create channel',
+        not_updated: 'Failed to update channel',
+        not_deleted: 'Failed to delete channel',
+        not_joined: 'Failed to join the channel',
+        not_left: 'Failed to leave the channel',
     }
 }
