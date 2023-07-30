a01f6dbb67fa9a72f7b0e8aa824a9b03664be047
Raiku <raikulmao.dev@gmail.com>
2023-07-30 22:53:07 +0300

    Update validation rules;

--- src/app/config/validation.ts
+++ src/app/config/validation.ts
@@ -2,18 +2,18 @@ const ALLOWED_LETTER_CHARS = 'а-яА-ЯёЁa-zA-Zàáâäãåąčćęèéêëė
 
 export const VALIDATION_RULES = {
     MESSAGE: {
-        MAX_LENGTH: 1000,
+        MAX_LENGTH: 1500,
         MIN_LENGTH: 2,
         REGEX: new RegExp(/[a-zA-Zа-яА-Я\d]{2,100}/gm),
     },
     NAME: {
-        MAX_LENGTH: 32,
+        MAX_LENGTH: 64,
         MIN_LENGTH: 2,
         REGEX: new RegExp(`^(?!.*-.*-.*-.*)(?!.*'.*'.*'.*)(?!.*\\s.*\\s.*\\s.*)[${ALLOWED_LETTER_CHARS}' -]+[${ALLOWED_LETTER_CHARS}]$`),
     },
     PASSWORD: {
-        MAX_LENGTH: 32,
-        MIN_LENGTH: 4,
+        MAX_LENGTH: 64,
+        MIN_LENGTH: 8,
         REGEX: new RegExp(/[^a-zA-Z0-9_!@#$%^&*]/g)
     },
     TITLE: {
